0f821edb919c5a3d4bd146a3e6eb42b2639cab23
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-02-09 20:38:29 +0300

    resoudre conflit

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,14 +1,11 @@
 <?php
 
 use app\controllers\ApiExampleController;
-<<<<<<< HEAD
-use app\controllers\ItemController;
-=======
 use app\controllers\CategoryController;
->>>>>>> ba04085a9f24499cfb7fd051cb3b973305a5515d
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
+use app\controllers\ItemController;
 
 /** 
  * @var Router $router 
@@ -18,27 +15,30 @@ use flight\net\Router;
 // This wraps all routes in the group with the SecurityHeadersMiddleware
 $router->group('', function (Router $router) use ($app) {
 
+    $router->get('/dashboard', function () use ($app) {
+        $app->render('dashboard');
+    });
+
+    $router->get('/login', function () use ($app) {
+        $app->render('login');
+    });
+
+    $router->get('/register', function () use ($app) {
+        $app->render('register');
+    });
+
+
 	$router->get('/', function () use ($app) {
 		$controller = new ItemController($app);
 			$app->render('index', ['items' => $controller->getAllItems()]);
 	});
 
-	$router->get('/login', function () use ($app) {
-		$app->render('login');
-	});
+	 $router->get('/categories', [CategoryController::class, 'renderCategoryList']);
+    $router->get('/categories/@id', [CategoryController::class, 'renderCategoryDetail']);
 
-	$router->get('/register', function () use ($app) {
-		$app->render('register');
-	});
-
-<<<<<<< HEAD
-=======
-	$router->get('/categories', [CategoryController::class, 'renderCategoryList']);
-
-	$router->group('/api', function () use ($router) {
-		$router->get('/users', [ApiExampleController::class, 'getUsers']);
-		$router->get('/users/@id:[0-9]', [ApiExampleController::class, 'getUser']);
-		$router->post('/users/@id:[0-9]', [ApiExampleController::class, 'updateUser']);
-	});
->>>>>>> ba04085a9f24499cfb7fd051cb3b973305a5515d
+    $router->group('/api', function () use ($router) {
+        $router->get('/users', [ApiExampleController::class, 'getUsers']);
+        $router->get('/users/@id:[0-9]', [ApiExampleController::class, 'getUser']);
+        $router->post('/users/@id:[0-9]', [ApiExampleController::class, 'updateUser']);
+    });
 }, [SecurityHeadersMiddleware::class]);
