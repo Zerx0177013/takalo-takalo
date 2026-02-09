0e34a6ffe22fc3687a1a289a9206a37a95972dd6
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-02-09 21:31:22 +0300

    Redirection login

--- app/config/routes.php
+++ app/config/routes.php
@@ -16,38 +16,47 @@ use app\controllers\ItemController;
 $router->group('', function (Router $router) use ($app) {
 
 	$router->group('/', function () use ($router, $app) {
-	
+
 		$authModel = new \app\models\AuthModel($app->db());
-		if(!$authModel->isLoggedIn()){
+		if (!$authModel->isLoggedIn()) {
+
+			$router->post('/login', function () use ($app) {
+				$email = $app->request()->data->email ?? null;
+				$password = $app->request()->data->password ?? null;
+			});
+
+			$router->get('/login', function () use ($app) {
 				$app->render('login');
-		} else{
-	$router->get('/dashboard', function () use ($app) {
-		$app->render('dashboard');
-	});
+			});
 
-	$router->post('/login', function () use ($app) {
-		$email = $app->request()->data->email ?? null;
-		$password = $app->request()->data->password ?? null;
-	});
+			$router->get('/register', function () use ($app) {
+				$app->render('register');
+			});
+		} else {
+			$router->get('/dashboard', function () use ($app) {
+				$app->render('dashboard');
+			});
 
-	$router->get('/register', function () use ($app) {
-		$app->render('register');
-	});
+			$router->post('/login', function () use ($app) {
+				$email = $app->request()->data->email ?? null;
+				$password = $app->request()->data->password ?? null;
+			});
 
 
-	$router->get('/', function () use ($app) {
-		$controller = new ItemController($app);
-		$app->render('index', ['items' => $controller->getAllItems()]);
-	});
 
-	$router->get('/propositions', [ItemController::class, 'propositions']);
+			$router->get('/', function () use ($app) {
+				$controller = new ItemController($app);
+				$app->render('index', ['items' => $controller->getAllItems()]);
+			});
 
-	$router->get('/items/new', function () use ($app) {
-		$app->render('item-new', ['categories' => []]);
-	});
+			$router->get('/propositions', [ItemController::class, 'propositions']);
 
-	$router->get('/categories', [CategoryController::class, 'renderCategoryList']);
-	$router->get('/categories/@id', [CategoryController::class, 'renderCategoryDetail']);
+			$router->get('/items/new', function () use ($app) {
+				$app->render('item-new', ['categories' => []]);
+			});
+
+			$router->get('/categories', [CategoryController::class, 'renderCategoryList']);
+			$router->get('/categories/@id', [CategoryController::class, 'renderCategoryDetail']);
 		}
 	});
 
