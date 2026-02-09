b6f92c75672fa3217cdb9c9e9a731bcc07f69742
Angeli <iriantsoaangeli@gmail.com>
2026-02-09 19:42:59 +0300

    LoginController , fonctio login

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -14,4 +14,16 @@ class UserModel
         return (int) $stmt->fetchColumn("total_users");
     }
 
+    public function checkEmail(string $email){
+            $stmt = $this->db->prepare('SELECT COUNT(*) FROM `user` WHERE email = :email');
+            $stmt->execute([':email' => $email]);
+            return (int) $stmt->fetchColumn() > 0;      
+    }
+
+    public function checkInfo($email , $password){
+        $stmt = $this->db->prepare('SELECT idUser FROM `user` WHERE email = :email AND password = :password');
+        $stmt->execute([':email' => $email, ':password' => $password]);
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+    
 }
