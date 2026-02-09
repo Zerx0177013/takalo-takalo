34591922fa284bb46f8357335449664bce336513
Angeli <iriantsoaangeli@gmail.com>
2026-02-09 17:09:03 +0300

    correction  UserModel , Fonction getNumberOfUsers

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,52 +1,18 @@
 <?php
-declare(strict_types=1);
 
 namespace app\models;
 
 class UserModel
 {
-    private int $idUser;
-    private string $username;
-    private string $email;
-    private string $password;
-
-    public function __construct(int $idUser, string $username, string $email, string $password)
+  private $db ;
+    public function __construct($db)
     {
-        $this->idUser = $idUser;
-        $this->username = $username;
-        $this->email = $email;
-        $this->password = $password;
+$this->db = $db;
+    }
+    public function getNumberOfUsers(): int
+    {
+        $stmt = $this->db->query('SELECT * FROM `V_COUNT_USERS`');
+        return (int) $stmt->fetchColumn("total_users");
     }
 
-    public static function fromArray(array $data): self
-    {
-        return new self(
-            $data['idUser'] ?? 0,
-            $data['username'] ?? '',
-            $data['email'] ?? '',
-            $data['password'] ?? ''
-        );
-    }
-
-    public function getId(): int
-    {
-        return $this->idUser;
-    }
-
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function getPasswordHash(): string
-    {
-        return $this->password;
-    }
 }
-
-?>
