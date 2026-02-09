eceedbfe39e627c81cd0c6fe46234435e0ab9016
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-02-09 20:39:07 +0300

    resoudre conflit

--- app/config/routes.php
+++ app/config/routes.php
@@ -36,9 +36,4 @@ $router->group('', function (Router $router) use ($app) {
 	 $router->get('/categories', [CategoryController::class, 'renderCategoryList']);
     $router->get('/categories/@id', [CategoryController::class, 'renderCategoryDetail']);
 
-    $router->group('/api', function () use ($router) {
-        $router->get('/users', [ApiExampleController::class, 'getUsers']);
-        $router->get('/users/@id:[0-9]', [ApiExampleController::class, 'getUser']);
-        $router->post('/users/@id:[0-9]', [ApiExampleController::class, 'updateUser']);
-    });
 }, [SecurityHeadersMiddleware::class]);
