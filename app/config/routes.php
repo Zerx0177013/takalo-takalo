1f5d5dbf41791dda06ec8f0077b74ffcf6f08bf0
LAM HINE YAN Randy Harry Narovana <198113444+Narovana69@users.noreply.github.com>
2026-02-09 21:00:50 +0100

    le dashboard
    fichier maina + routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,7 +1,6 @@
 <?php
 
 use app\controllers\ApiExampleController;
-use app\controllers\CategoryController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
@@ -14,6 +13,14 @@ use flight\net\Router;
 // This wraps all routes in the group with the SecurityHeadersMiddleware
 $router->group('', function (Router $router) use ($app) {
 
+	$router->get('/', function () use ($app) {
+		$app->render('dashboard');
+	});
+
+	$router->get('/coucou', function () use ($app) {
+		$app->render('welcome', ['message' => 'Coucouuuuuuuuuuuuuuuuuuuuuuuuuuuu!']);
+	});
+
 	$router->get('/login', function () use ($app) {
 		$app->render('login');
 	});
@@ -22,8 +29,10 @@ $router->group('', function (Router $router) use ($app) {
 		$app->render('register');
 	});
 
-	$router->get('/categories', [CategoryController::class, 'renderCategoryList']);
-	
+	$router->get('/hello-world/@name', function ($name) {
+		echo '<h1>Hello world! Oh hey ' . $name . '!</h1>';
+	});
+
 	$router->group('/api', function () use ($router) {
 		$router->get('/users', [ApiExampleController::class, 'getUsers']);
 		$router->get('/users/@id:[0-9]', [ApiExampleController::class, 'getUser']);
