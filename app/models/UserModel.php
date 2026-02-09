9bbeed572ff65ad6322104e06fc5ac8329fe5aa5
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-02-09 20:12:27 +0300

    Login de l'admin

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\models;
 
 class UserModel
@@ -14,7 +13,6 @@ class UserModel
         $stmt = $this->db->query('SELECT * FROM `V_COUNT_USERS`');
         return (int) $stmt->fetchColumn("total_users");
     }
-<<<<<<< HEAD
 
     public function checkEmail(string $email){
             $stmt = $this->db->prepare('SELECT COUNT(*) FROM `user` WHERE email = :email');
@@ -23,11 +21,21 @@ class UserModel
     }
 
     public function checkInfo($email , $password){
-        $stmt = $this->db->prepare('SELECT idUser FROM `user` WHERE email = :email AND password = :password');
+        $stmt = $this->db->prepare('SELECT idUser,username FROM `user` WHERE email = :email AND password = :password');
         $stmt->execute([':email' => $email, ':password' => $password]);
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
+        $user = $stmt->fetch(\PDO::FETCH_ASSOC);
+        if($this->isAdmin($user['idUser'])){
+            $user['isAdmin'] = true;
+        }
+        else{
+            $user['isAdmin'] = false;
+        }
+        return $user ?: null; 
+    }
+    public function isAdmin($id){
+        $stmtm = $this->db->prepare('SELECT COUNT(*) FROM `admin` WHERE idUser = :id');
+        $stmtm->execute([':id' => $id]);
+        return (int) $stmtm->fetchColumn() > 0;
     }
     
-=======
->>>>>>> 77f42277f9d00822d7cae239f052e6e677b307b0
 }
