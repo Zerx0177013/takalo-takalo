5d5e836de48fb509eb11717bd68aed69fcc3f115
Rohan <andriamihaja2007rohan@gmail.com>
2026-02-10 10:23:15 +0300

    fixed merge conflict

--- app/config/routes.php
+++ app/config/routes.php
@@ -25,13 +25,8 @@ $router->group('', function (Router $router) use ($app) {
 			$router->post('/login', function () use ($app) {
 				$email = $app->request()->data->email ?? null;
 				$password = $app->request()->data->password ?? null;
-<<<<<<< HEAD
-				$authController = new \app\controllers\AuthController($app);
-				$authController->login($email, $password);
-=======
 				$authController = new AuthController($app);
 				$user = $authController->login($email, $password);
->>>>>>> aa58b72ddb808bdab6e2ed8df806764405ac7e8e
 				if ($authController->isLogged())
 					$app->redirect('/dashboard');
 				else
@@ -69,7 +64,7 @@ $router->group('', function (Router $router) use ($app) {
 			$router->get('/propositions', [ItemController::class, 'propositions']);
 			$router->get('/my-items', [ItemController::class, 'myItems']);
 
-			
+
 
 			$router->get('/items/new', function () use ($app) {
 				$app->render('item-new', ['categories' => []]);
