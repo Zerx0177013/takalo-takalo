406f3b410379116af05b5aced0c04dd84aa6ab04
LAM HINE YAN Randy Harry Narovana <198113444+Narovana69@users.noreply.github.com>
2026-02-15 00:50:14 +0100

    mise en place des etu dans footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <div class="container-fluid d-flex justify-content-between">
         <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright 2026</span>
-        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> Angeli ETU000000</span>
-        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> Rohan ETU000000</span>
-        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> Randy ETU000000</span>
+        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> Angeli ETU003964</span>
+        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> Rohan ETU003918</span>
+        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> Randy ETU004101</span>
     </div>
 </footer>
